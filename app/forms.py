b9eff29d4cbb3bc3058b21de9ccb5f9dd4fb3fd9
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-06-17 21:41:11 +0530

    Added review form

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,20 @@ from app.models import Movie, Review
 
 class MovieForm(forms.ModelForm):
 
-	class Meta:
-		model = Movie
-		fields = ("name", "description", "poster")
-		widgets = {
+    class Meta:
+        model = Movie
+        fields = ("name", "description", "poster")
+        widgets = {
             'name': forms.TextInput(attrs={'placeholder': 'Movie Name'}),
             'description': forms.Textarea(attrs={'placeholder': 'Movie Description'}),
+        }
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ("rating", "review")
+        widgets = {
+            'review': forms.Textarea(attrs={'placeholder': 'Write a Review'}),
         }
