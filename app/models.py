60c4770015f4ec2d1d966afd014fd9db69fc637b
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-06-17 13:07:05 +0530

    Fixed avg. rating and review count func

--- app/models.py
+++ app/models.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 from app.utils import get_upload_file_path
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Movie(models.Model):
-	added_by = models.ForeignKey(User)
+	added_by = models.ForeignKey(User, blank=True, null=True)
 	name = models.CharField(max_length=150)
 	description = models.TextField(blank=True, null=True)
 	poster = models.ImageField(upload_to=get_upload_file_path, null=True, blank=True)
@@ -14,8 +16,17 @@ class Movie(models.Model):
 	def __str__(self):
 		return self.name
 
+	@property
+	def avg_ratings_and_reviews_count(self):
+		ratings = self.review_set.values_list('rating', flat=True)
+		length = len(ratings)
+		if length == 0:
+			return 0, 0
+		return reduce(lambda x, y: x + y, ratings) / (length * 1.0), length
 
-class Reviews(models.Model):
+
+
+class Review(models.Model):
 	STATUS_CHOICES = (
 		(1, _("*")),
 		(2, _("**")),
@@ -29,4 +40,4 @@ class Reviews(models.Model):
 	review = models.TextField()
 
 	def __str__(self):
-		return self.rating
+		return self.review
