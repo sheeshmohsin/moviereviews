9dd7e5a8dc11418166490e552f273ab666908b07
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-06-17 21:41:52 +0530

    Added views for review

--- app/views.py
+++ app/views.py
@@ -6,9 +6,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import NoReverseMatch, reverse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from app.models import Movie, Review
-from app.forms import MovieForm
+from app.forms import MovieForm, ReviewForm
 
 # Create your views here.
 def home(request):
@@ -28,24 +28,58 @@ def home(request):
 
 def movie(request, pk):
     if request.method == 'POST':
-        if request.user.is_authenticated():
-            pass
+        movie = Movie.objects.get(pk=pk)
+        form_obj = ReviewForm(request.POST)
+        if form_obj.is_valid():
+            db_obj = form_obj.save(commit=False)
+            db_obj.movie = movie
+            if request.user.is_authenticated():
+                db_obj.added_by = request.user
+            db_obj.save()
+            pk_value = db_obj.pk
+            return HttpResponseRedirect(reverse('movie_detail', args=(pk,)))
+        else:
+            reviews = paginate_reviews(request, movie)
+            return render(
+                            request, 'movie.html', 
+                            {'movie': movie, 'form': form, 'reviews': reviews}
+                        )
     elif request.method == 'GET':
         movie = Movie.objects.get(pk=pk)
-        return render(request, 'movie.html', {'movie': movie})
+        form = ReviewForm()
+        reviews = paginate_reviews(request, movie)
+        return render(request, 'movie.html', {'movie': movie, 'form': form, 'reviews': reviews})
     else:
-        raise Http404("Poll does not exist")
+        raise Http404("Request method type not defined")
 
 @login_required
 def add_movie(request):
     if request.method == 'POST':
-        form_obj = MovieForm(request.POST)
+        form_obj = MovieForm(request.POST, request.FILES)
         if form_obj.is_valid():
-            form_obj.added_by = request.user
-            form_obj.save()
-            pk_value = form_obj.id
-            return reverse('movie_detail', args=(pk_value,))
+            db_obj = form_obj.save(commit=False)
+            db_obj.added_by = request.user
+            db_obj.save()
+            pk_value = db_obj.pk
+            return HttpResponseRedirect(reverse('movie_detail', args=(pk_value,)))
         else:
             return render(request, 'add_movie.html', {'form': form_obj})
     form = MovieForm()
     return render(request, 'add_movie.html', {'form': form})
+
+
+def paginate_reviews(request, movie):
+    review_list = Review.objects.filter(movie=movie)
+    paginator = Paginator(review_list, 2) # Show 2 movies per page
+
+    page = request.GET.get('page')
+    try:
+        reviews = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        reviews = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        reviews = paginator.page(paginator.num_pages)
+    return reviews
+
