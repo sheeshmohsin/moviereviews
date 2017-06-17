d4a2c12cda89fdaa8e83fb0a6b1fe79bff5a4547
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-06-17 13:06:36 +0530

    Added tables in admin panel

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from app.models import Movie, Review
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Review)
